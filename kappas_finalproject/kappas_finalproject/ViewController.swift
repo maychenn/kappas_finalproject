b177ef552a65b7f3fb1863bedf48b9c34f3e3731
May <may99chen@gmail.com>
2019-11-11 14:51:17 -0600

    ver 1
    only 1st view

---
+++ kappas_finalproject/kappas_finalproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kappas_finalproject
+//
+//  Created by May Chen on 11/11/19.
+//  Copyright © 2019 May Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
